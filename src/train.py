689c2d675718d7f5060a1f97cf0a24f21640f5dc
riazus <riyaz@alexlegal.fr>
2025-02-16 19:03:53 +0100

    feat: configurable early_step

--- src/train.py
+++ src/train.py
@@ -1,6 +1,5 @@
 import argparse
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 from utils import feed_forward, sigmoid, load
 
@@ -27,13 +26,11 @@ def plot_learning_curves(train_losses, valid_losses,
     plt.show()
 
 
-def get_stopper():
+def get_stopper(patience):
 	counter = 0
 	best_loss = None
 	best_epoch = None
-
 	improvement_threshold = 0.001
-	patience = 5
 	
 	def check_stopping(valid_loss, epoch):
 		nonlocal counter, best_loss, best_epoch
@@ -72,7 +69,7 @@ def get_accuracy(y_pred, y_true):
         return np.mean(predictions == y_true)
 
 
-def sparse_categorical_cross_entropy(y_true, y_pred):
+def binary_cross_entropy(y_true, y_pred):
 	epsilon = 1e-15
 	m = y_true.shape[0]
 	y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
@@ -131,12 +128,13 @@ def init(layers):
 	return weights, biases
 
 
-def main(layers, epochs, learning_rate, batch_size):
+def main(layers, epochs, learning_rate, 
+		 batch_size, early_stop_status, patience):
 	train_losses = []
 	valid_losses = []
 	train_accuracies = []
 	valid_accuracies = []
-	check_early_stopping = get_stopper()
+	check_early_stopping = get_stopper(patience)
 
 	y_train, X_train = get_processed_data("train")
 	y_valid, X_valid = get_processed_data("valid")
@@ -161,8 +159,8 @@ def main(layers, epochs, learning_rate, batch_size):
 		train_output, _ = feed_forward(X_train, weights, biases)
 		valid_output, _ = feed_forward(X_valid, weights, biases)
 
-		train_loss = sparse_categorical_cross_entropy(y_train, train_output)
-		valid_loss = sparse_categorical_cross_entropy(y_valid, valid_output)
+		train_loss = binary_cross_entropy(y_train, train_output)
+		valid_loss = binary_cross_entropy(y_valid, valid_output)
 
 		train_accuracy = get_accuracy(train_output, y_train)
 		valid_accuracy = get_accuracy(valid_output, y_valid)
@@ -179,7 +177,7 @@ def main(layers, epochs, learning_rate, batch_size):
                   f" - val_acc: {valid_accuracy:.4f}")
 
 		early_stop, best_epoch = check_early_stopping(valid_loss, epoch)
-		if (early_stop == True):
+		if (early_stop == True and early_stop_status == 1):
 			print(f"Early stop detected. Best epoch was: {best_epoch}.")
 			break
 	
@@ -189,27 +187,39 @@ def main(layers, epochs, learning_rate, batch_size):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='Training')
-    parser.add_argument('--layers', 
-                        help='Layers and count of neurons in format: "24 24 24". By default "24 24 24"', 
-                        nargs='+', type=int,
-                        default=[24, 24, 24],
-                        required=False)
-    parser.add_argument('--epochs', 
-                        help='Count of the epochs. By default "80"',
+	parser = argparse.ArgumentParser(prog='MLP Training')
+	parser.add_argument("-l", "--layers", 
+						help='Layers and count of neurons in format: "24 24 24". By default "24 24 24"', 
+						nargs='+', type=int,
+						default=[24, 24, 24],
+						required=False)
+	parser.add_argument("-e", '--epochs', 
+						help='Count of the epochs. By default 80',
 						type=int,
-                        default=80,
-                        required=False)
-    parser.add_argument('--learning_rate', 
-                        help='Learning rate. By default 0.0314',
-                        type=float,
+						default=80,
+						required=False)
+	parser.add_argument("-r", '--learning_rate', 
+						help='Learning rate. By default 0.0314',
+						type=float,
 						default=0.0314,
-                        required=False)
-    parser.add_argument('--batch_size', 
-                        help='Feature count in batch. By default 8',
-                        type=int,
+						required=False)
+	parser.add_argument("-s", '--batch_size', 
+						help='Feature count in batch. By default 8',
+						type=int,
 						default=8,
-                        required=False)
+						required=False)
+	parser.add_argument('--early_stop', 
+						help='Enable an early stop. By default 1',
+						type=int,
+						default=1,
+						choices=[1, 0],
+						required=False)
+	parser.add_argument("-p", "--patience", 
+						help='Number of epochs before early stop. By default 5',
+						type=int,
+						default=5,
+						required=False)
 
-    args = parser.parse_args()
-    main(args.layers, args.epochs, args.learning_rate, args.batch_size)
+	args = parser.parse_args()
+	main(args.layers, args.epochs, args.learning_rate, 
+	  args.batch_size, args.early_stop, args.patience)
